82823c5ca489af76d2212c9da9d9f27f4992e15c
oleg-ilin <75135418+oleg-ilin@users.noreply.github.com>
2021-09-28 04:51:25 +0300

    Update script04_.js

--- script04_.js
+++ script04_.js
@@ -7,32 +7,44 @@ HTML-код должен содержать только div id=”catalog” б
 */
 
 
-product = {
-    obj1 :
-    {  
-        name : 'джинсы',
-        brand : 'levis',
-        size : 50,
-        price : 9000
-    },
-    obj2 :
-    {
-        name : 'свитер',
-        brand : 'gas',
-        size : 50,
-        price : 10000
-    },
-    obj3 :
-    {
-        name : 'футболка',
-        brand : 'H&M',
-        size : 50,
-        price : 10000
-    },
-    giveMeCatalog : function(){
-        console.log('Тест')
-
-            }
+let product = {
+    parts :
+      [
+      {type: 'Процессор',
+      brand: 'intel',
+      model: 'Pentium 4500',
+      price: 1000},
+      {type: 'Видеокарта',
+      brand: 'nVidia',
+      model: 'GForce440',
+      price: 2000},
+      {type: 'SSD-накопитель',
+      brand: 'Kingston',
+      model: 'V300',
+      price: 3000},
+      ],
+      giveCatalog : function(){
+        var header = document.createElement('div_0'); 
+        header.innerHTML = '<h1>Каталог</h1>'
+        document.body.appendChild(header);
+        var catalog = document.createElement('div-1');
+        catalog.className = 'div_1'    
+        document.body.appendChild(catalog);
+        for (el in product.parts){
+          var cart = document.createElement('div-2');
+          cart.className = 'div_2'
+          item = (`Комлектующее: 
+          ${product.parts[el]['type']} ${product.parts[el]['brand']} ${product.parts[el]['model']},
+          цена: ${product.parts[el]['price']} руб.`)
+          cart.innerHTML = item;
+          catalog.appendChild(cart);
         }
-product.giveMeCatalog()
+      }
+    }
+  
+  
+product.giveCatalog()
+
+
+
 
